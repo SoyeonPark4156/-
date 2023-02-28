cee4c0aa0e52a8b1b7c8e1049e5552e20d965a32
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-02-28 22:36:38 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,13 @@
+function solution(num1, num2) {
+    if(num1 >= 0 && num1 <= 10000 && num2 >= 0 && num2 <= 10000){
+        if(num1 === num2){
+            return 1;
+        }
+        else if(num1 !== num2){
+            return -1;
+        }
+    }
+    else{
+        conosle.log("잘못된 입력입니다.")
+    }
+}
