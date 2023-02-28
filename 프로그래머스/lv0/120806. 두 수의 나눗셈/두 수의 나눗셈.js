b40d9da6b83fbc8a4883b496ebac005a74e09425
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-02-28 22:32:34 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+    if(num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100){
+        var answer = parseInt((num1 / num2) * 1000);
+        return answer;
+    }
+    else{
+        conosle.log("잘못된 입력입니다.")
+    }
+    
+}
