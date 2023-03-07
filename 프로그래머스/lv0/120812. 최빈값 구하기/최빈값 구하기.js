28ae178163f506068542b588ed267828ff306665
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-03-07 20:25:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.67 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,9 +1,28 @@
 function solution(array) {
-    let arr = new Map();
-    for(let i = 0; i < array.length; i++){
-        arr.set(array[i], (arr.get(array[i])||0) + 1);
+    let map = new Map();
+    //Map 객체 생성
+    
+    // Map 객체 초기화
+    for(let i = 0; i <= Math.max(...array); i++){
+        map.set(i, 0);    
     }
-    let max = Math.max(...[...arr].map(v => v[1]))
-    let count = [...arr].filter(v => v[1] === max).length
-    return count > 1 ? -1 : [...arr].find(v => v[1] === max)[0]
+    
+    // array 배열의 원소값과 일치하는 Map 객체의 key를 증가시킨다
+    for(let i = 0; i < array.length; i++){
+        map.set(array[i], map.get(array[i]) + 1);
+        //map객체에 array[i]를 키로 , array[i]값을 가져와서 거기에 +1 해주고 값으로 set
+    }
+    
+    // Map 객체의 value만 모아서 배열로 만든다
+    let arr = Array.from(map.values());
+    
+    // 최대값 산출
+    let max = Math.max(...arr);
+    
+    // 최대값이 하나만 있다면 max, 여러 개 중복된다면 -1 출력
+    if(arr.indexOf(max) !== arr.lastIndexOf(max)){
+        return -1;
+    } else {
+        return arr.indexOf(max);
+    }
 }
