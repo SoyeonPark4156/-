ac4fc3b7c0465697d0f2019d6d38efe1d6d537d8
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-03-08 17:50:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -10,3 +10,10 @@ function solution(emergency) {
 //이때 emergency는 얕은복사를 해줬기 때문에 변하지 않는다
 //arr의 길이만큼 for문 돌려서 arr에서 emergency의 배열의 요소와 일치하는 요소의 index값을 찾아줌
 // 이때 index는 0부터 시작하기 때문에 마지막에 +1을 해주었다.
+
+
+// function solution(emergency) {
+//     let sorted = emergency.slice().sort((a,b)=>b-a);
+//     return emergency.map(v=>sorted.indexOf(v)+1);
+// }
+// 기존의 배열에서 map을 돌려서 정렬한 배열의 요소값의 index값을 찾는 방법! 
