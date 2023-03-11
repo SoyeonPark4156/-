c1335ebfd68256879233479d9fb96363d470abcb
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-03-11 22:59:37 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,17 @@
+function solution(dot) {
+    if(dot[0] * dot[1] > 0){
+        if(dot[1] > 0){
+            return 1;
+        }else{
+            return 3;
+        }
+    }
+    else{
+        if(dot[1] > 0){
+            return 2;
+        }
+        else{
+            return 4;
+        }
+    }
+}
