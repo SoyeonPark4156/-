1998bad586b3e657c50af9f6445c78ba2139c40c
soyeon park <118035343+SoyeonPark4156@users.noreply.github.com>
2023-03-13 21:39:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,9 +1,9 @@
 function solution(n) {
     var answer = [];
     let count = 0;
-    for(let i = 1; i<=n;i++){
+    for(let i = 1; i<=n;i++){//i는 1부터 n까지의 숫자들
         let count = 0;
-            for(let j=1;j<=i;j++){
+            for(let j=1;j<=i;j++){ //약수찾기, j는 i를 나눌 숫자들
                 if(i%j == 0){
                     count++;
                 }
